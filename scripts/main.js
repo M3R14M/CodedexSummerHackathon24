eb531e4ed584b994eed16b80b8599f811c972798
AlexandruSoroiu <alexandru.soroiu@wagner.edu>
2024-07-12 13:47:31 +0200

    countdown
    changes in the html, css, and main.js files to implement a countdown until the stoop sale starts

--- scripts/main.js
+++ scripts/main.js
@@ -17,3 +17,25 @@ var popup = L.popup()
     .setLatLng([40.6795092, -73.9972808])
     .setContent("Come to our Stoop Sale for amazing vibes and great deals.<br> 2nd Pl & Court St · Brooklyn, NY 11231")
     .openOn(map);
+
+dayjs.extend(dayjs_plugin_duration);
+
+// activation of the countdown
+function activateCountdown(element, dateString) {
+    const targetDate = dayjs(dateString);
+
+    setInterval(() => {
+        const now = dayjs();
+        const duration = dayjs.duration(targetDate.diff(now));
+
+        // sort out the case where the date has passed
+        if (duration.asMilliseconds() <= 0) return;
+
+        element.querySelector(".until__numeric--seconds").textContent = duration.seconds().toString().padStart(2, "0");
+        element.querySelector(".until__numeric--minutes").textContent = duration.minutes().toString().padStart(2, "0");
+        element.querySelector(".until__numeric--hours").textContent = duration.hours().toString().padStart(2, "0");
+        element.querySelector(".until__numeric--days").textContent = duration.asDays().toFixed(0).toString().padStart(2, "0");
+    }, 250);
+}
+
+activateCountdown(document.getElementById("myCountdown"), "2024-07-13");
