b3e434a1c532a2c6bc38d67e62d33de0dbc61c7e
M3R14M <ex.inferi.rs3@gmail.com>
2024-07-12 17:28:16 +0200

    Restored lost files

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,22 @@
 // Creates a map at a set longitude and latitude
-var map = L.map('map').setView([40.6795092, -73.9972808], 18);
-
+const map = L.map('map').setView([40.6795092, -73.9972808], 18);
 
 // Creates a OpenStreetMap tile layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  maxZoom: 18,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-
 // Creates a pin at a set longitude and latitude
-var marker = L.marker([40.6794082, -73.9972808]).addTo(map);
+const marker = L.marker([40.6794082, -73.9972808]).addTo(map);
+document.querySelector('.leaflet-marker-icon').src =
+  '../assets/marker-icon.png';
 
 // Creates a text bubble at a set longitude and
-var popup = L.popup()
-    .setLatLng([40.6795092, -73.9972808])
-    .setContent("Come to our Stoop Sale for amazing vibes and great deals.<br> 2nd Pl & Court St · Brooklyn, NY 11231")
-    .openOn(map);
+const popup = L.popup()
+  .setLatLng([40.6795092, -73.9972808])
+  .setContent(
+    'Come to our Stoop Sale for amazing vibes and great deals.<br> 2nd Pl & Court St · Brooklyn, NY 11231'
+  )
+  .openOn(map);
