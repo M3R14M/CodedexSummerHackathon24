a067ca6d6a9e666b576649dc8c3cb8fec537db3d
0xKonstantin <119298586+0xKonstantin@users.noreply.github.com>
2024-07-11 21:31:42 -0700

    Deleted unnecessary script files and added a map with a pin and text bubble at the stoop sale location

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+// Creates a map at a set longitude and latitude
+var map = L.map('map').setView([40.6795092, -73.9972808], 18);
 
+
+// Creates a OpenStreetMap tile layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 18,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+// Creates a pin at a set longitude and latitude
+var marker = L.marker([40.6795092, -73.9972808]).addTo(map);
+
+// Creates a text bubble at a set longitude and latitude
+var popup = L.popup()
+    .setLatLng([40.679618, -73.997349])
+    .setContent("Come to our Stoop Sale for amazing vibes and great deals.<br> 2nd Pl & Court St · Brooklyn, NY 11231")
+    .openOn(map);
